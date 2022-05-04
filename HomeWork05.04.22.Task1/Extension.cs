e52235729491a2b9d30df395e2175c2020e084f7
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-05-04 17:34:38 +0400

    added home work

---
+++ HomeWork05.04.22.Task1/Extension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork05._04._22.Task1
+{
+   static  class Extension
+    {
+        public static void ContainsMethod(this string name, string value)
+        {
+            bool result = false;
+            string Chek = string.Empty;
+            for (int i = 0; i < name.Length; i++)
+            {
+                if ((i + value.Length) > name.Length)
+                {
+                    break;
+                }
+                Chek = name.Substring(i, value.Length);
+                if (Chek.ToLower() == value.ToLower())
+                {
+                    result = true;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HomeWork05.04.22.Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork05._04._22.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Bu bir custom contains methodudur";
+            name.ContainsMethod("U Bi");
+        }
+
+
+    }
+}
+
